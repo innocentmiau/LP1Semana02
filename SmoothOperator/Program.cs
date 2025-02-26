b799a0c96f3d95a1aa839ffa193242a0d27277bd
André <imandreleandro@gmail.com>
2025-02-26 14:32:35 +0000

    Added a SmoothOperator that converts a string into a sbyte variable and decreases and increases it's value by 1

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insere número inteiro:");
+            sbyte n = Convert.ToSByte(Console.ReadLine());
+            
+            Console.WriteLine($"Number: {--n}");
+            Console.WriteLine($"Number: {++n}");
+        }
+    }
+}
