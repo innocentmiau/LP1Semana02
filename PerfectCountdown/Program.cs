c7155fb039f27129c12a5dcfbf88649326ec43d8
André <imandreleandro@gmail.com>
2025-02-28 14:15:16 +0000

    PerfectCountdown test

---
+++ PerfectCountdown/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PerfectCountdown
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Start number:");
+                int start = int.Parse(Console.ReadLine());
+                Console.WriteLine("Step number:");
+                int step = int.Parse(Console.ReadLine());
+                if (start <= 1 || step <= 1)
+                {
+                    Console.WriteLine("Out-of-range start or step. Try again.");
+                    continue;
+                }
+                if (step >= start)
+                {
+                    Console.WriteLine("Start must be higher than step. Try again.");
+                    continue;
+                }
+                if (start % step != 0)
+                {
+                    Console.WriteLine("Start not divisible by step. Try again");
+                    continue;
+                }
+
+                for (int i = start; i >= 0; i -= step)
+                {
+                    Console.WriteLine($"{i}");
+                }
+            }
+        }
+    }
+}
